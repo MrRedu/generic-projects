5b62d67d2b8de8a276f5bfd71357f284b4d703e9
Eduardo <ejrb1234@gmail.com>
2025-05-03 19:58:03 -0400

    move media to public

--- 42/countdown-to-events/src/components/commons/templates/header.tsx
+++ 42/countdown-to-events/src/components/commons/templates/header.tsx
@@ -32,7 +32,7 @@ export const Header = () => {
       <header>
         <div className="flex items-center justify-between px-4 py-2  border-b  max-w-7xl mx-auto">
           <h1 className="text-2xl font-bold sr-only">CountdownApp</h1>
-          <img src="../../../assets/icon.svg" alt="Logo" className="w-8 h-8" />
+          <img src="/icon.svg" alt="Logo" className="w-8 h-8" />
           <div className="flex items-center gap-1">
             <Button size="icon" variant="ghost">
               <Bell />
