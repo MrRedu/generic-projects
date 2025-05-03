f68d72fac692115e91b595716c13e2c44874888b
Eduardo <ejrb1234@gmail.com>
2025-05-03 14:55:30 -0400

    emptyState events

--- 42/countdown-to-events/src/components/commons/organisms/events.tsx
+++ 42/countdown-to-events/src/components/commons/organisms/events.tsx
@@ -5,20 +5,19 @@ import { Event } from '../molecules/event';
 export const Events = () => {
   const events = useEventsStore((state) => state.events);
 
+  if (events.length === 0) {
+    return (
+      <div className="flex items-center justify-center w-full min-h-80 h-full">
+        <p className="text-gray-500">No hay eventos agendados.</p>
+      </div>
+    );
+  }
+
   return (
-    <>
-      <EventsLayout>
-        {events.map((event) => (
-          <Event
-            key={event.id}
-            id={event.id}
-            title={event.title}
-            theme={event.theme}
-            targetDate={event.targetDate}
-            reminder={event.reminder}
-          />
-        ))}
-      </EventsLayout>
-    </>
+    <EventsLayout>
+      {events.map((event) => (
+        <Event key={event.id} {...event} />
+      ))}
+    </EventsLayout>
   );
 };
