dcc50bc817997cc0ef6e22e46e007ae4a7591f40
Eduardo <ejrb1234@gmail.com>
2025-05-03 14:33:08 -0400

    feat: capitalize function

--- 42/countdown-to-events/src/lib/utils.ts
+++ 42/countdown-to-events/src/lib/utils.ts
@@ -1,6 +1,9 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const capitalize = (string: string): string =>
+  string.charAt(0).toUpperCase() + string.slice(1);
